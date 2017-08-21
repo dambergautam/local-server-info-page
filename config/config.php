f4143e65b4ea093949dd23bbf4015cfeccbd6b02
Damber Prasad Gautam <damber.gautam@gmail.com>
2017-08-21 12:56:40 +1000

    updated gitIgnore

--- config/config.php
+++ config/config.php
@@ -1,10 +1,1 @@
-<?php return array(
-				'VHOST' => '/etc/apache2/extra/httpd-vhosts.conf',
-				'HOSTS' => '/etc/hosts',
-				'PROJECT_DIR' => '/Users/damber/Sites',
-				'DB' => array(
-					'USER' => 'root',
-					'PASSWORD' => 'Prakash123',
-					'HOST' => 'localhost',
-					),
-			);
+
